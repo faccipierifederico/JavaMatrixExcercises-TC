c719e7ca393fbe4c743cb670d21735344933a978
Federico Faccipieri <federicofaccipieri@gmail.com>
2023-08-10 15:58:19 -0300

    Excercise 4, done

--- src/Main.java
+++ src/Main.java
@@ -61,7 +61,7 @@ public class Main {
         // Realizar 3 vectores diferentes. En el primero colocar ciudades, y en los otros dos temperaturas mínimas y máximas respectivamente.
         // Luego mostrar por pantalla qué ciudad tubo T mínima y cuál la máxima.
 
-        String cities[] = new String[5];
+/*        String cities[] = new String[5];
         Double min[] = new Double[5];
         Double max[] = new Double[5];
 
@@ -102,7 +102,51 @@ public class Main {
         System.out.println("The minium T was: " + minium);
         System.out.println("And happened in: " + cities[posMin]);
         System.out.println("The maximum T was: " + maximum);
-        System.out.println("And happened in: " + cities[posMax]);
+        System.out.println("And happened in: " + cities[posMax]);*/
 
+        // EXC 4
+        // La idea es hacer una app para una empresa de viajes con 6 destinos y en 3 turnos disponibles. La gente tiene que ir y sacar sus vuelos.
+
+        int flights[][] = new int [6][3];
+
+        Scanner scanner = new Scanner(System.in);
+
+        for (int r=0; r<6; r++) {
+            for (int c=0; c<3; c++) {
+                System.out.println("Write the number of seats available for the destination: " + r + " time: " + c);
+                flights[r][c] = scanner.nextInt();
+            }
+        }
+
+        Scanner scanner1 = new Scanner(System.in);
+        String flag = "";
+        int destination, time, seats;
+
+        while (!flag.equalsIgnoreCase("finish")) {
+            System.out.println("Write the N° of the destination");
+            destination = scanner.nextInt();
+            System.out.println("Write the time of the flight");
+            time = scanner.nextInt();
+            System.out.println("Write the amount of seats");
+            seats = scanner.nextInt();
+
+            if (destination >= 0 && destination <= 5) {
+                if (time >= 0 && time <= 2) {
+                    if (flights[destination][time] >= seats) {
+                        System.out.println("Your reservation was a succes");
+                        flights[destination][time] = flights[destination][time] - seats;
+                    } else {
+                        System.out.println("There is no seat available");
+                    }
+                } else {
+                    System.out.println("The time of the flight no exists. Choose between 0 and 2");
+                }
+            } else {
+                System.out.println("The destination of the flight no exists. Choose between 0 and 5");
+            }
+
+            System.out.println("Do you want to continue buying? If you want, write anything. If you don't, write 'finish'.");
+            flag = scanner1.next();
+        }
      }
 }
